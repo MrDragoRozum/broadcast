2383cd33fe99a589d8cd0726dd5de7a3fa2560f6
Alexander Rozum <roozumaleks@gmail.com>
2023-10-17 16:44:35 +0300

    Создан первый receiver

---
+++ app/src/main/java/com/example/broadcast/MyReceiver.kt
@@ -0,0 +1,16 @@
+package com.example.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when(intent?.action) {
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(context, "Мало зарядки!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
