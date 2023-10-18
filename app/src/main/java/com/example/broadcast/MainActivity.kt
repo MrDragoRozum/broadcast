1bb7dd2d2a620a903fd4d9bde13244f5efe180b1
Alexander Rozum <roozumaleks@gmail.com>
2023-10-18 12:40:20 +0300

    Создан и отправлен собственный бродкаст

--- app/src/main/java/com/example/broadcast/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -3,24 +3,34 @@ package com.example.broadcast
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private val receiver = MyReceiver()
+    private var countClick = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val filter = IntentFilter().apply {
-//            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-//            addAction(Intent.ACTION_BATTERY_CHANGED)
-//        }
-//        registerReceiver(receiver, filter)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Intent(MyReceiver.ACTION_CLICKED).apply {
+                putExtra(MyReceiver.EXTRA_CLICKED, ++countClick)
+                sendBroadcast(this)
+            }
+        }
+
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            addAction(MyReceiver.ACTION_CLICKED)
+        }
+        registerReceiver(receiver, filter)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-//        unregisterReceiver(receiver)
+        unregisterReceiver(receiver)
     }
 }
--- app/src/main/java/com/example/broadcast/MyReceiver.kt
+++ app/src/main/java/com/example/broadcast/MyReceiver.kt
@@ -8,6 +8,10 @@ import android.widget.Toast
 class MyReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when(intent?.action) {
+            ACTION_CLICKED -> {
+                val clicks = intent.getIntExtra(EXTRA_CLICKED, 0)
+                Toast.makeText(context, "Клик! $clicks", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
                 val turnedOn = intent.getBooleanExtra("state", false)
                 Toast.makeText(context, "Авиарижм: $turnedOn", Toast.LENGTH_LONG).show()
@@ -16,5 +20,10 @@ class MyReceiver: BroadcastReceiver() {
                 Toast.makeText(context, "Зарядка", Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    companion object {
+        const val ACTION_CLICKED = "clicked"
+        const val EXTRA_CLICKED = "click"
     }
 }
